e4b74171818d24e6cc0a52f166086c2d007078d3
Betsy Haibel <betsy.haibel@gmail.com>
2011-07-21 21:51:04 -0400

    Carta::Stitch distinguishes between valid and invalid stitch names

--- carta.rb
+++ carta.rb
@@ -1,5 +1,15 @@
 module Carta
   class Stitch
+    STITCH_ABBREVIATIONS = [:sc, :hdc, :dc, :tr]
+    
     attr_reader :slug
+    
+    def initialize(a_slug)
+      if STITCH_ABBREVIATIONS.include?(a_slug)
+        @slug = a_slug
+      else
+        @slug = :INVALID_STITCH
+      end
+    end
   end
 end
--- carta_spec.rb
+++ carta_spec.rb
@@ -2,10 +2,17 @@ require 'carta.rb'
 
 describe Carta::Stitch do
   it 'has a slug' do
-    stitch = Carta::Stitch.new
+    stitch = Carta::Stitch.new(nil)
     stitch.respond_to?(:slug).should be_true
   end
   
-  it 'accepts valid crochet stitch abbreviations as slugs'
-  it 'does not accept invalid crochet stitch abbreviations as slugs'
+  it 'accepts valid crochet stitch abbreviations as slugs' do
+    stitch = Carta::Stitch.new(:dc)
+    stitch.slug.should == :dc
+  end
+  
+  it 'does not accept invalid crochet stitch abbreviations as slugs' do
+    stitch = Carta::Stitch.new(:abracadabra)
+    stitch.slug.should == :INVALID_STITCH
+  end
 end
